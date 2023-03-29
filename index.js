678088d4963eae633cbaa6bee3de9fba93570949
marianoarguello10 <124093841+marianoarguello10@users.noreply.github.com>
2023-03-29 17:08:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Funcion que redirecciona entre paginas 
 const rendirectAbout =()=>{
 
-    let nuevaURL = 'http://127.0.0.1:5500/sobre.html'; // La URL de destino.
+    let nuevaURL = 'http://marianoarguello10.github.io/sobre.html'; // La URL de destino.
     let espera   =  1; // La espera en segundos antes de ejecurtar la redirección.
    
     //Mostrar un mensaje sobre que el contenido cambio de ubicación y esperar x segundos para que sea leído.
@@ -20,7 +20,7 @@ const rendirectAbout =()=>{
 })}
 const rendirectContact =()=>{
 
-    let nuevaURL = 'http://127.0.0.1:5500/contacto.html'; // La URL de destino.
+    let nuevaURL = 'http://marianoarguello10.github.io/contacto.html'; // La URL de destino.
     let espera   =  1; // La espera en segundos antes de ejecurtar la redirección.
    
     function paginaCargaCompleta(fn) {
